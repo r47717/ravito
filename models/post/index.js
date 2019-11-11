1c10a67b6bea7efc14871b72cdd8fed57894fdc1
r47717 <mchr@yandex.ru>
2019-11-11 19:06:02 +0300

    basic post fetch by category

---
+++ models/post/index.js
@@ -0,0 +1,47 @@
+const Sequelize = require('sequelize');
+const { createConnection } = require('../../lib/db');
+const sequelize = createConnection();
+
+class Post extends Sequelize.Model {
+}
+
+Post.init({
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    contacts: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'active'
+    },
+    category: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    modelName: 'post'
+    // options
+});
+
+Post.categoryNames = {
+    'auto': "Авто",
+    'real-estate': "Недвижимость",
+    'job': 'Работа',
+    'service': 'Услуги'
+};
+
+
+module.exports = Post;
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const Post = require('../models/post');
 
 router.get('/', async function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const posts = await Post.findAll();
+  res.render('index', { posts });
 });
+
+router.get('/post/category/:category', async function(req, res, next) {
+  const { params: { category } } = req;
+
+  const posts = await Post.findAll({
+    where: {
+      category
+    }
+  });
+  res.render('index', {
+    posts,
+    category: Post.categoryNames[category]
+  });
+});
+
+router.get('/post/load-test-data', async function(req, res, next) {
+  await Post.sync({ force: true });
+
+  const data = require('../models/post/fake-posts');
+  data.forEach(async function(item) {
+    await Post.create({
+      title: item.title,
+      description: item.description,
+      contacts: item.contacts,
+      status: item.status || "active",
+      category: item.category
+    });
+  });
+  res.render('index');
+});
+
 
 module.exports = router;
